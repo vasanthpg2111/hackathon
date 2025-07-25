be91b222a2b881c1fc134d2c70689da6f56e7692
unknown <vasanthpg2111@gmail.com>
2025-07-25 12:12:53 +0530

    changes are updated

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function showSection(id) {
+  document.querySelectorAll('section').forEach(s => s.classList.remove('visible'));
+  document.getElementById(id).classList.add('visible');
+}
 
+function handleRentalType(type) {
+  if (type === 'driver') {
+    showSection('driver-select-section');
+  } else {
+    showSection('payment-section');
+  }
+}
